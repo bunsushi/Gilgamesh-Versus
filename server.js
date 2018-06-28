a5c3925d51b86008371150a8d62727f3af27e317
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-06-28 19:55:31 -0400

    Require dependencies and define middleware

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,6 +13,20 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(logger("dev"));
+
+// Define Passport here
+app.use(session({
+  secret: "keyboard cat",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+// Load Passport strategies
+require("./config/passport/passport.js")(passport);
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -16,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gilgameshversus");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gilgameshversus");
 
 // Start the API server
 app.listen(PORT, function() {
