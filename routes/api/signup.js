b13eb2b0559b1b871c9f5a5b43117c30db4d44bd
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-06-28 21:08:02 -0400

    Add POST route for signup

--- routes/api/signup.js
+++ routes/api/signup.js
@@ -1,9 +1,28 @@
 const router = require("express").Router();
+const passport = require("passport");
+const Account = require("../../models/account");
 
 // Matches with "/api/signup"
 router.route("/")
     .get(function(req, res) {
         res.send("Route working");
+    })
+    .post(function(req, res) {
+        Account.register(new Account({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            username: req.body.username
+        },
+        req.body.password,
+        function(err, account) {
+            if (err) {
+                return res.json(err);
+            }
+            passport.authenticate("local", {
+                successRedirect: "/menu",
+                failureRedirect: "/"
+            });
+        }));
     });
 
 module.exports = router;
