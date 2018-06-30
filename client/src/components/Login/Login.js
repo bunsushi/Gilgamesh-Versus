235992877af73ee7c46e4938eab8da693410f7f8
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-06-30 11:06:33 -0400

    Convert Login component to class

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Login = props => (
-    <div className="login">
-        <form id="signup" name="signup" method="POST" action="/signup">
-            <div className="form-options">
-                <div className="form-pill" id="login"><h1>log in</h1></div>
-                <div className="form-pill" id="sign-up"><h1>sign up</h1></div>
+class Login extends Component {
+
+    state = {
+        firstname: "",
+        lastname: "",
+        username: "",
+        password: ""
+    };
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    render() {
+        return (
+            <div className="login">
+                <form id="signup" name="signup" method="POST" action="/api/signup">
+                    <div className="form-options">
+                        <div className="form-pill" id="login"><h1>log in</h1></div>
+                        <div className="form-pill" id="sign-up"><h1>sign up</h1></div>
+                    </div>
+                    <div className="form-group">
+                        <label></label>
+                        <input className="form-control" type="text" id="username" name="username" placeholder="username" value={this.state.username} onChange={this.handleInputChange} />
+                        <br />
+                        <label></label>
+                        <input className="form-control" type="password" id="password" name="password" placeholder="password" value={this.state.password} onChange={this.handleInputChange} />
+                        <br />
+                        <input className="btn" type="submit" id="btn-login" value="Play!" />
+                    </div>
+                </form>
             </div>
-            <div className="form-group">
-                <label></label>
-                <input type="text" id="username" name="username" placeholder="username" />
-                <br />
-                <label></label>
-                <input type="password" id="password" name="password" placeholder="password" />
-                <br />
-                <input className="btn" type="submit" id="btn-login" value="Play!" />
-            </div>
-        </form>    </div>
-);
+        );
+    }
+}
 
 export default Login;
