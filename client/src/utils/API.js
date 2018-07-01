d4c0fca0cd0bc1f7e4e49b4b0a66fe5bbf658f39
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-07-01 19:25:35 -0400

    Add POST route for signin

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,6 +5,10 @@ export default {
     signupUser: function(signupData) {
         return axios.post("/api/signup", signupData);
     },
+    // Authenticates an existing user in the DB
+    signinUser: function(signinData) {
+        return axios.post("/api/signin", signinData);
+    },
     // Signs out the current user
     signoutUser: function() {
         return axios.get("/api/signout");
--- routes/api/signin.js
+++ routes/api/signin.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
+const passport = require("passport");
 
 // Matches with "/api/signin"
 router.route("/")
     .get(function(req, res) {
         res.send("Route also working");
+    })
+    .post(passport.authenticate("local"), function(req, res) {
+        res.json(true);
     });
 
 module.exports = router;
