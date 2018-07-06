a7ebf41ac143b1f7c2aa90f2b9c2d19936fc1901
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-07-05 20:15:03 -0400

    Create fakeAuth object to simulate auth API

--- client/src/App.js
+++ client/src/App.js
@@ -16,4 +16,16 @@ const App = () => (
   </Router>
 );
 
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true;
+    setTimeout(cb, 100); // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false;
+    setTimeout(cb, 100); // fake async
+  }
+};
+
 export default App;
