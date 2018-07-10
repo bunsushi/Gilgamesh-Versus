8d7996bc6121d71b069e218b5d2c3094cdd281e9
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-07-09 22:27:56 -0400

    Build user achievement API routes

---
+++ routes/api/achievements.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const achievementsController = require("../../controllers/achievementsController");
+
+// Matches with "/api/achievements"
+router.route("/")
+    .get(achievementsController.findAll)
+    .post(achievementsController.create);
+
+// Matches with "/api/achievements/:id"
+router.route("/:id")
+    .get(achievementsController.findById)
+    .put(achievementsController.update)
+    .delete(achievementsController.remove);
+
+module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -3,11 +3,13 @@ const signupRoutes = require("./signup");
 const signinRoutes = require("./signin");
 const signoutRoutes = require("./signout");
 const checkRoutes = require("./check");
+const achievementRoutes = require("./achievements");
 
 // Define routes here
 router.use("/signup", signupRoutes);
 router.use("/signin", signinRoutes);
 router.use("/signout", signoutRoutes);
 router.use("/check", checkRoutes);
+router.use("/achievements", achievementRoutes);
 
 module.exports = router;
