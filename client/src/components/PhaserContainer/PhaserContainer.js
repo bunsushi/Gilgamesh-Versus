0fe127c18b0c1740e5e716aa7b457ecfb1e752cb
bunsushi <tbunsu@gmail.com>
2018-07-11 20:51:04 -0400

    added phaser 3 upload function for moving cat and fly

--- client/src/components/PhaserContainer/PhaserContainer.js
+++ client/src/components/PhaserContainer/PhaserContainer.js
@@ -3,6 +3,16 @@ import Phaser from "phaser";
 
 class PhaserContainer extends Component {
 
+    constructor(props) {
+        super(props)
+
+        // this.preload = this.preload.bind(this);
+        // this.create = this.create.bind(this);
+        // this.update = this.update.bind(this);
+        this.startGame = this.startGame.bind(this);
+
+    }
+
     componentDidMount() {
         this.startGame();
     }
@@ -17,7 +27,7 @@ class PhaserContainer extends Component {
                 default: 'arcade',
                 arcade: {
                     gravity: { y: 800 },
-                    debug: false
+                    debug: true
                 }
             },
             scene: {
@@ -26,26 +36,98 @@ class PhaserContainer extends Component {
                 create: this.create,
                 update: this.update
             }
-        };    
-    
+        };
+
         this.game = new Phaser.Game(config);
-    
+
     }
 
     preload() {
+        // map made with Tiled in JSON format
+        this.load.tilemapTiledJSON('map', 'assets/game/maps/map.json');
+        // tiles in spritesheet
+        this.load.spritesheet('ground-tileset', 'assets/game/maps/ground-tileset.png', { frameWidth: 70, frameHeight: 70 });
 
-        this.load.image('sky', 'assets/images/sky.png');
+        // simple Gilgamesh cat
+        this.load.image('gilgamesh', 'assets/game/character/gilgamesh.png');
+        // // enemy fly spritesheet
+        this.load.spritesheet('enemy', 'assets/game/npc/fly-spritesheet.png', { frameWidth: 70, frameHeight: 40 });
     }
 
     create() {
 
-        this.add.image(400, 300, 'sky');
+        let map;
+        let groundLayer, sandLayer;
+        let player;
+        let cursors;
 
+        // load Tiled map
+        map = this.make.tilemap({ key: 'map' });
+        // load tiles for ground layer
+        let groundTiles = map.addTilesetImage('ground-tileset');
+        groundLayer = map.createDynamicLayer('Ground', groundTiles, 0, 0);
         this.cameras.main.setBackgroundColor('#b4c3d1');
-    }
+
+        groundLayer.setCollisionByExclusion([-1]);
+
+        // // set the boundaries of our game world
+        this.physics.world.bounds.width = groundLayer.width;
+        this.physics.world.bounds.height = groundLayer.height;
+
+        // // create the player sprite    
+        this.player = this.physics.add.sprite(50, 50, 'gilgamesh');
+        this.player.setBounce(0.1); // our player will bounce from items
+        this.player.setCollideWorldBounds(true); // don't go out of the map
+        this.player.immune = false;
+
+        // // small fix to our player images, we resize the physics body object slightly
+        this.player.body.setSize(this.player.width - 60, this.player.height - 8);
+
+        // // player will collide with the level tiles 
+        this.physics.add.collider(groundLayer, this.player);
+
+        // // create the fly animation
+        this.fly = this.physics.add.sprite(50, 550, 'enemy');
+        this.fly.setCollideWorldBounds(true);
+        this.fly.body.setVelocityX(100);
+
+        // // adjust fly to be above the ground slightly
+        this.fly.body.setSize(this.fly.width, this.fly.height + 20);
+
+        // enemy will collide with the level tiles
+        this.physics.add.collider(groundLayer, this.fly);
+
+        this.anims.create({
+            key: 'fly',
+            frames: this.anims.generateFrameNumbers('enemy', { start: 0, end: 1 }),
+            frameRate: 7,
+            repeat: -1
+        });
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+    };
 
     update() {
 
+        this.fly.anims.play('fly', true);
+
+        // move player left
+        if (this.cursors.left.isDown) {
+            this.player.body.setVelocityX(-200);
+            this.player.flipX = true; // flip the sprite to the left
+        }
+        // move player right
+        else if (this.cursors.right.isDown) {
+            this.player.body.setVelocityX(200);
+            this.player.flipX = false; // use the original sprite looking to the right
+        } else {
+            this.player.body.setVelocityX(0);
+        }
+        // jump 
+        if (this.cursors.up.isDown && this.player.body.onFloor()) {
+            this.player.body.setVelocityY(-500);
+        }
     };
 
     render() {
