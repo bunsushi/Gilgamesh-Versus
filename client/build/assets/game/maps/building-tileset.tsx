accfa14c2779841d9628f0ec6ff5b1eca0e994f8
Theron Halsey <theron.halsey@gmail.com>
2018-07-17 18:56:10 -0400

    minus build

--- client/build/assets/game/maps/building-tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="building-tileset" tilewidth="70" tileheight="70" tilecount="60" columns="5">
- <image source="building-tileset.png" trans="ff00ff" width="350" height="840"/>
-</tileset>
