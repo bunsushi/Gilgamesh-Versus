6c788d752ac03731dad6629c60da8cd4c8a2e67a
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-07-16 21:30:59 -0400

    Revert using renderContent method; use state to proceedToLoad

--- client/src/components/PrivateRoute/PrivateRoute.js
+++ client/src/components/PrivateRoute/PrivateRoute.js
@@ -3,34 +3,49 @@ import { Route, Redirect } from "react-router-dom";
 import API from "../../utils/API";
 
 class PrivateRoute extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isAuthenticated: false,
+            proceedToLoad: false
+        };
+    }
+
+    mounted = false;
+
+    async componentDidMount() {
+        this.mounted = true;
+        const isAuthed = await this.getAuthStatus();
+        if (this.mounted) this.setState({ isAuthenticated: isAuthed });
+    }
+
+    componentWillUnmount() {
+        this.mounted = false;
+    }
 
     async getAuthStatus() {
         let res = await API.getAuthStatus();
         console.log(res);
         let returnVal = await res.data;
         console.log(returnVal);
+        this.setState({ proceedToLoad: true });
         return returnVal;
     }
-
-    renderContent(isAuthenticated) {
+    
+    render() {
         const { path, component } = this.props;
 
-        if (isAuthenticated) {
+        if (this.state.isAuthenticated && this.state.proceedToLoad) {
             return (
                 <Route exact path={path} component={component} />
             );
         }
         else {
             return (
-                <Redirect to="/" />
+                // <Redirect to="/" />
+                <div>Not isAuthenticated!!</div>
             );
         }
-    };
-
-    render() {
-        const isAuthenticated = this.getAuthStatus();
-        const content = this.renderContent(isAuthenticated);
-        return content;
     }
 }
 
