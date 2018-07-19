d4932793eb8bf168c180fecd220133be416ac77a
Theron Halsey <theron.halsey@gmail.com>
2018-07-19 01:32:18 -0400

    more sound effects and background music, without build folder

--- client/build/assets/game/maps/danger.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="danger" tilewidth="70" tileheight="70" tilecount="4" columns="4">
- <image source="danger.png" trans="ff00ff" width="280" height="70"/>
-</tileset>
